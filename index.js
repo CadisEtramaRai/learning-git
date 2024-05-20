312b2ae4657467b521dca2005d63f60caa85d1bd
CadisEtramaRai <cadisetrama2010@gmail.com>
2024-05-20 16:41:16 +0100

    pushing the third change

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
-const number = document.querySelector("span");
+const number = document.querySelector(".number");
+const substract = document.querySelector(".substraction");
+const add = document.querySelector(".addition");
+const span = document.querySelector(".number2");
 
 let count = 0;
 plus.addEventListener("click", function () {
@@ -10,5 +13,9 @@ plus.addEventListener("click", function () {
 minus.addEventListener("click", function () {
   count--;
   number.innerHTML = count;
+});
+add.addEventListener("click", function () {
+  count--;
+  span.innerHTML = count;
 });
 /* this is an updated version to test version control and pulling the project */
